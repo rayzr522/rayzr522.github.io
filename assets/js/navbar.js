2dfe0decfa5a015b94394103059ca89c9789519e
Rayzr522 <rayzr522@gmail.com>
2019-08-08 21:21:44 -0400

    Added in animation to navbar pulldown tray

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,15 +1,9 @@
+Iconify.preloadImages(['mdi-expand-more', 'mdi-expand-less']);
+
 const navbarPulldown = document.querySelector('.navbar-pulldown');
-const navbarMore = navbarPulldown.children[0];
-const navbarLess = navbarPulldown.children[1];
 const navbar = document.getElementById('nav');
 
-let navbarOpen = false;
-
 navbarPulldown.addEventListener('click', () => {
-    navbarOpen = !navbarOpen;
-
-    navbar.style.display = navbarOpen ? '' : 'none';
-
-    navbarMore.style.display = navbarOpen ? 'none' : '';
-    navbarLess.style.display = navbarOpen ? '' : 'none';
+    navbar.classList.toggle('open');
+    navbarPulldown.classList.toggle('open');
 });
