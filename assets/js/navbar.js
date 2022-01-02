b665655753eb71056a698148b046fa00f890945d
rayzr522 <rayzr522@gmail.com>
2022-01-01 20:07:27 -0500

    simplify that shit

--- assets/js/navbar.js
+++
@@ -1,7 +0,0 @@
-const navbarPulldown = document.querySelector('.navbar-pulldown');
-const navbar = document.getElementById('nav');
-
-navbarPulldown.addEventListener('click', () => {
-    navbar.classList.toggle('open');
-    navbarPulldown.classList.toggle('open');
-});
